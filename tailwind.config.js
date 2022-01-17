0b09183844dfda3554cb0dbbeca7e75f405b6bc9
Agency <agency@hhsdtp5134simac.wireless.hhs.nl>
2022-01-17 17:14:15 +0100

    working on recent

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,6 +64,13 @@ module.exports = {
       width: {
         'initial': 'initial',
       },
+      zIndex: {
+        '-10': '-10',
+        '-20': '-20',
+        '-30': '-30',
+        '-40': '-40',
+        '-50': '-50',
+      }
     },
     container: {
       padding: {
