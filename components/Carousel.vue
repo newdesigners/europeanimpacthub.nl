93ef91861e594f87bcac9baa0ca244fb97692c1d
Agency <agency@hhsdtp5134simac.wireless.hhs.nl>
2022-01-21 15:22:48 +0100

    adjust carousel for image and icons

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -5,21 +5,33 @@
       class="pb-14 lg:pb-20 relative w-full"
     >
     <div class="w-full max-w-[100%] overflow-hidden">
-      <h2 class="text-20 lg:text-36 pb-5 lg:pb-8 text-center">Ontmoet het Team</h2>
+      <h2 class="text-20 lg:text-36 pb-5 lg:pb-8 text-center">{{ blok.title }}</h2>
       <VueSlickCarousel v-bind="slickOptions">
         <div
-          v-for="(slide, index) in slides"
+          v-for="(slide, index) in blok.slides"
           :key="index"  
         >
           <div class="mx-3 w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] lg:mx-8 text-15 text-center">
-            <figure class="w-full h-full rounded-md overflow-hidden mb-2.5 drop-shadow-team-member ">
+            <figure
+              class="w-full h-full rounded-md overflow-hidden mb-2.5"
+              :class="{ 'drop-shadow-team-member' : blok.type === 'images' }"
+            >
               <img
-                class="w-full h-full object-cover"
+                class="w-full h-full"
+                :class="{
+                  'object-cover' : blok.type === 'images',
+                  'object-contain' : blok.type === 'icons',
+                }"
                 :src="slide.image.filename"
                 :alt="slide.image.alt"
               />
             </figure>
-            <p class="font-medium text-black lg:text-20">{{ slide.name }}</p>
+            <p
+              v-if="slide.name !== ''" 
+              class="font-medium text-black lg:text-20"
+            >
+              {{ slide.name }}
+            </p>
             <p class="text-black lg:text-20">{{ slide.title }}</p>
           </div>
         </div>
@@ -32,6 +44,11 @@
 
 <script>
 export default {
+  props: {
+    blok: {
+      type: Object,
+    },
+  },
   data() {
     return {
       slickOptions: {
@@ -43,6 +60,7 @@ export default {
         infinite: true,
         variableWidth: true,
         autoplay: true,
+        autoplaySpeed: 5000,
         pauseOnHover: true,
       },
       slides: [
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,82 @@
     <TitleText />
     <MenuCards />
     <RecentNewsPreview />
-    <Carousel />
+    <Carousel
+      :blok="{
+        type: 'images',
+        title: 'Ontmoet het Team',
+        slides: [
+          {
+            name: 'Tibor Hargitai',
+            title: 'Onderzoeker',
+            image: {
+              filename: 'https://picsum.photos/200',
+              alt: 'some alt text',
+            },
+          },
+          {
+            name: 'Tibor Hargitai',
+            title: 'Onderzoeker',
+            image: {
+              filename: 'https://picsum.photos/201',
+              alt: 'some alt text',
+            },
+          },
+          {
+            name: 'Tibor Hargitai',
+            title: 'Onderzoeker',
+            image: {
+              filename: 'https://picsum.photos/202',
+              alt: 'some alt text',
+            },
+          },
+          {
+            name: 'Tibor Hargitai',
+            title: 'Onderzoeker',
+            image: {
+              filename: 'https://picsum.photos/203',
+              alt: 'some alt text',
+            },
+          },
+        ],
+      }"
+    />
+    <Carousel
+      :blok="{
+        type: 'icons',
+        title: 'Onze Partners',
+        slides: [
+          {
+            title: 'Partner Name',
+            image: {
+              filename: require('~/assets/images/logo-example.svg'),
+              alt: 'some alt text',
+            },
+          },
+          {
+            title: 'Partner Name',
+            image: {
+              filename: require('~/assets/images/logo-example-2.svg'),
+              alt: 'some alt text',
+            },
+          },
+          {
+            title: 'Partner Name',
+            image: {
+              filename: require('~/assets/images/logo-example.svg'),
+              alt: 'some alt text',
+            },
+          },
+          {
+            title: 'Partner Name',
+            image: {
+              filename: require('~/assets/images/logo-example.svg'),
+              alt: 'some alt text',
+            },
+          },
+        ],
+      }"
+    />
   </section>
 </template>
 
