99e0bbde0e212dd65e6695cfb76562090c0a2a0c
Agency <agency@hhsdtp5134simac.wireless.hhs.nl>
2022-01-21 16:21:19 +0100

    add component for page introduction

---
+++ components/PageIntro.vue
@@ -0,0 +1,21 @@
+<template>
+  <article
+    class="bg-cover w-full min-h-[150px] lg:min-h-[464px] h-full relative bg-primary overflow-hidden mb-5 lg:mb-10 2xl:mb-20"
+    :style="{ background: `linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.75)), url(${require(`~/assets/images/image-horizontal.jpg`)})`}"
+  >
+    <div class="container">
+      <h1 class="text-primary pt-6 pb-4 lg:pb-8 lg:pt-16 lg:text-50">{{ blok.title }}</h1>
+      <p class="text-white font-bold w-2/3 pb-10 lg:w-[70%] lg:text-30">{{ blok.description }}</p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+    },
+  },
+};
+</script>
---
+++ pages/kennis-en-onderzoek.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+  <PageIntro
+    :blok="{
+      image: {
+        filename: ''
+      },
+      title: 'Kennis & Onderzoek',
+      description: 'Als lectoraat op de Haagse Hogeschool doen wij onderzoek naar de praktijk van Europees werken.'
+    }"
+  />
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
