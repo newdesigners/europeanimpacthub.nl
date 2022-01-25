c769c4091f76da4975547928bd93dfc6c506716c
Agency <agency@hhsdtp5134simac.wireless.hhs.nl>
2022-01-25 17:07:44 +0100

    started single post

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,85 @@
+<template>
+  <ol vocab="http://schema.org/" typeof="BreadcrumbList">
+    <!-- <li property="itemListElement" typeof="ListItem">
+      <NuxtLink property="item" typeof="WebPage" to="/">
+        <span property="name">Home</span>
+      </NuxtLink>
+      <meta property="position" content="1" />
+    </li> -->
+    <li 
+      v-for="(crumb, index) in crumbs"
+      :key="index"
+      property="itemListElement"
+      typeof="ListItem"
+    >
+      <NuxtLink property="item" typeof="WebPage" :to="crumb.path">
+        <span property="name">{{
+          $route.fullPath === crumb.path && title !== null ? title : crumb.title
+        }}</span>
+      </NuxtLink>
+      <meta property="position" :content="index + 2" />
+    </li>
+  </ol>
+</template>
+
+<script>
+const titleCase = require('ap-style-title-case');
+export default {
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    crumbs() {
+      const fullPath = this.$route.fullPath;
+      let params = fullPath.startsWith('/')
+        ? fullPath.substring(1).split('/')
+        : fullPath.split('/')
+      const crumbs = [];
+      let path = '';
+      
+      params = params.filter((p)=> {
+        return p !== '';
+      });
+      params.forEach((param, index) => {
+        path = `${path}/${param}`
+        const match = this.$router.match(path);
+        if (match.name !== null) {
+          crumbs.push({
+            title: titleCase(param.replace(/-/g, ' ')),
+            ...match,
+          })
+        }
+      })
+      return crumbs;
+    },
+  }
+}
+</script>
+
+<style scoped>
+ol {
+  @apply list-none pb-1;
+}
+
+li {
+  @apply inline text-black;
+}
+
+li:after {
+  content: ' > ';
+  @apply inline text-black;
+}
+li:last-child:after {
+  content: '';
+}
+li a {
+  @apply text-black no-underline;
+}
+
+li a.nuxt-link-exact-active.nuxt-link-active {
+  @apply underline;
+}
+</style>
---
+++ pages/diensten/_slug.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container" v-editable="blok">
+    <div class="post-single__section">
+      <aside class="text-15 pb-5 lg:pb-9">
+        <Breadcrumb />
+      </aside>
+      <article class="">
+        <figure class="w-full rounded-xl overflow-hidden">
+          <img v-if="blok.image" :src="blok.image.filename" :alt="blok.image.alt" class="post-single__src">
+        </figure>
+        <div class="md:w-3/5">
+          <h1 class="text-center">{{ blok.title }}</h1>
+          <p>{{ blok.excerpt }}</p>
+          <div class="">       
+          </div>
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
+ 
+<script>
+export default {
+  data() {
+    return {
+      blok: {
+        title: 'Beleidsonderzoek & evaluatie',
+        excerpt: 'Met één been in de wereld van onderzoek & onderwijs, met het andere been in de werkpraktijk: dat kenmerkt de teamleden van European Impact Hub. Deze brugfunctie geeft ons de ideale uitgangspositie om kennis in te zetten voor het verbeteren of veranderen van Europees werken. We doen dit voor overheden, ondernemingen en belangenorganisaties.',
+        image: {
+          filename: 'https://picsum.photos/1350/430',
+          alt: 'some alt text here',
+        },
+      },
+    };
+  },
+  // props: {
+  //   blok: {
+  //     type: Object,
+  //     required: true,
+  //   },
+  // },
+};
+</script>
