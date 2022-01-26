ce2e46a5961fe36571be7869589161cf70367627
Steven Wu <master_msp1@hotmail.com>
2022-01-26 16:54:33 +0100

    single post md

--- components/SinglePost.vue
+++ components/SinglePost.vue
@@ -4,7 +4,7 @@
       <Breadcrumb />
     </aside>
     <article class="">
-      <figure class="w-full rounded-xl overflow-hidden relative bg-primary pt-[45%] lg:pt-[32%] overflow-hidden mb-2.5 lg:mb-8">
+      <figure class="w-full rounded-xl overflow-hidden relative bg-primary pt-[45%] lg:pt-[32%] overflow-hidden mb-2.5 md:mb-8">
         <img class="block max-h-full max-w-full h-full absolute top-0 left-0 right-0 bottom-0 object-cover object-center"
           v-if="blok.image"
           :src="blok.image.filename"
@@ -12,10 +12,10 @@
         />
       </figure>
       <div class="md:mx-auto md:w-3/5">
-        <h1 class="text-black lg:text-center text-18 lg:text-35 pb-1.5 lg:mb-3">
+        <h1 class="text-black md:text-center text-18 md:text-35 pb-1.5 md:mb-3">
           {{ blok.title }}
         </h1>
-        <p class="text-black font-medium pb-8 lg:pb-16">{{ blok.excerpt }}</p>
+        <p class="text-black font-medium pb-8 md:pb-16 md:text-20 md:font-normal">{{ blok.excerpt }}</p>
         <div class="rich-text rich-text--post">
           <rich-text-renderer
             v-if="content.body"
